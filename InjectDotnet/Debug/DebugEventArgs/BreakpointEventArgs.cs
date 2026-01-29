54a68fd99f43c410fc33879831164ce1ac8ef6d2
MVSharp <zxcnio93@gmail.com>
2026-01-30 01:22:54 +0800

    fix(ci): unignore InjectDotnet/Debug folder and add missing source files

---
+++ InjectDotnet/Debug/DebugEventArgs/BreakpointEventArgs.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using InjectDotnet.Native;
+
+namespace InjectDotnet.Debug.DebugEventArgs;
+
+public enum BreakType
+{
+    Other = 0,
+
+    /// <summary>
+    /// The system breakpoint. Raised after the loader has run but before the entry point.
+    /// </summary>
+    System,
+
+    /// <summary>
+    /// Breakpoint at the debugee's entry point.
+    /// </summary>
+    EntryPoint,
+
+    /// <summary>
+    /// A user-defined memory breakpoint. See <see cref="UserBreakpoint"/>.
+    /// </summary>
+    UserMemory,
+}
+
+/// <summary>
+/// Provides exception information for the <see cref="Debugger.Breakpoint"/> event.
+/// </summary>
+public class BreakpointEventArgs : ContinuableDebuggerEventArgs, IException
+{
+    public nint Address     { get; }
+    public bool FirstChance { get; }
+    public bool Handled     { get; set; } = true;
+
+    /// <summary>
+    /// The type of breakpoint encountered.
+    /// </summary>
+    public BreakType Type { get; }
+
+    /// <summary>
+    /// If <see cref="Type"/> is <see cref="BreakType.UserMemory"/>, the <see cref="UserBreakpoint"/> that was hit.
+    /// </summary>
+    public UserBreakpoint? Breakpoint { get; }
+
+    internal BreakpointEventArgs(BreakType type, DebugEvent debugEvent, UserBreakpoint? breakpoint = null)
+        : base(debugEvent)
+    {
+        Type        = type;
+        Address     = debugEvent.u.Exception.ExceptionRecord.ExceptionAddress;
+        FirstChance = debugEvent.u.Exception.dwFirstChance != 0;
+        Breakpoint  = breakpoint;
+    }
+
+    public override string ToString()
+    {
+        return $"{Type} Break: 0x" + Address.ToString($"x{IntPtr.Size * 2}");
+    }
+}
---
+++ InjectDotnet/Debug/DebugEventArgs/OutputDebugStringEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using InjectDotnet.Native;
+
+namespace InjectDotnet.Debug.DebugEventArgs;
+
+/// <summary>
+/// Provides data for the <see cref="Debugger.OutputDebugString"/> event.
+/// </summary>
+public class OutputDebugStringEventArgs : ContinuableDebuggerEventArgs
+{
+    /// <summary>
+    /// The debugging string output by the debugee.
+    /// </summary>
+    public string? DebugString { get; }
+
+    internal OutputDebugStringEventArgs(IMemoryAccess memoryAccess, DebugEvent debugEvent)
+        : base(debugEvent)
+    {
+        DebugString = debugEvent.u.OutputDebugString.ReadMessageFromTarget(memoryAccess);
+    }
+}
